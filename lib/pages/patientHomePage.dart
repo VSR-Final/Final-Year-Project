169bfbe9c530df03460e50c6679612d3638cabb6
Veron Ramkissoon <veronramkissoon@gmail.com>
2023-02-25 22:44:16 -0400

    added calendar functionality

--- lib/pages/patientHomePage.dart
+++ lib/pages/patientHomePage.dart
@@ -2,23 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class PatientHomePage extends StatefulWidget {
-  const PatientHomePage({super.key});
+  const PatientHomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<PatientHomePage> {
-  String _selectedOption = 'Schedule';
-  DateTime _selectedDate = DateTime.now();
-  DateTime _lastDay =
-      DateTime.utc(2023, 12, 31); // define the last day of the calendar
+  late CalendarFormat _calendarFormat;
+  late DateTime _focusedDay;
+  late DateTime _selectedDay;
+  late Map<DateTime, List<String>> _events;
+
+  @override
+  void initState() {
+    super.initState();
+    _calendarFormat = CalendarFormat.month;
+    _focusedDay = DateTime.now();
+    _selectedDay = DateTime.now();
+    _events = {
+      DateTime(2022, 2, 20): ['Event 1'],
+      DateTime(2022, 2, 23): ['Event 2', 'Event 3'],
+      DateTime(2022, 3, 1): ['Event 4'],
+      DateTime(2022, 3, 3): ['Event 5'],
+      DateTime(2022, 3, 7): ['Event 6'],
+      DateTime(2022, 3, 14): ['Event 7'],
+    };
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Calendar Demo'),
       ),
       drawer: Drawer(
         child: ListView(
@@ -32,88 +55,66 @@ class _HomePageState extends State<PatientHomePage> {
             ),
             ListTile(
               title: Text('Schedule'),
-              onTap: () {
-                setState(() {
-                  _selectedOption = 'Schedule';
-                });
-                Navigator.pop(context);
-              },
+              onTap: () {},
             ),
             ListTile(
               title: Text('Patients'),
-              onTap: () {
-                setState(() {
-                  _selectedOption = 'Patients';
-                });
-                Navigator.pop(context);
-              },
+              onTap: () {},
             ),
             ListTile(
               title: Text('Chats'),
-              onTap: () {
-                setState(() {
-                  _selectedOption = 'Chats';
-                });
-                Navigator.pop(context);
-              },
+              onTap: () {},
             ),
           ],
         ),
       ),
-      body: _selectedOption == 'Schedule'
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TableCalendar(
-                  focusedDay: _selectedDate,
-                  firstDay: DateTime.utc(2021),
-                  lastDay: _lastDay,
-                  calendarFormat: CalendarFormat.month,
-                  onDaySelected: (date, events) {
-                    setState(() {
-                      _selectedDate = date;
-                    });
-                  },
-                  availableCalendarFormats: {
-                    CalendarFormat.month: 'Month',
-                  },
-                  headerStyle: HeaderStyle(
-                    titleTextStyle: TextStyle(fontSize: 20),
-                    formatButtonVisible: false,
-                  ),
-                  calendarStyle: CalendarStyle(
-                    selectedDecoration: BoxDecoration(
-                      color: Colors.blue,
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  startingDayOfWeek: StartingDayOfWeek.sunday,
-                  onPageChanged: (focusedDay) {
-                    if (isSameDay(focusedDay, _lastDay) ||
-                        focusedDay.isBefore(_lastDay)) {
-                      setState(() {
-                        _selectedDate = focusedDay;
-                      });
-                    }
-                  },
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: Text('This is the schedule.'),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          : _selectedOption == 'Patients'
-              ? Center(
-                  child: Text('This is the patients page.'),
-                )
-              : Center(
-                  child: Text('This is the chats page.'),
-                ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2022, 1, 1),
+            lastDay: DateTime.utc(2022, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            eventLoader: _getEventsForDay,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            onDaySelected: _onDaySelected,
+            calendarStyle: CalendarStyle(
+              markersMaxCount: 2,
+              todayDecoration: BoxDecoration(
+                color: Colors.blueAccent,
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.circle,
+              ),
+              markerDecoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+            headerStyle: HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _events[_selectedDay]?.length ?? 0,
+              itemBuilder: (context, index) {
+                final event = _events[_selectedDay]![index];
+                return ListTile(
+                  title: Text(event),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  List<String> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
   }
 }
