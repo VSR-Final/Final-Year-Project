f89c72ec5f4515a62b69b170c8b49e1af117f85b
Veron Ramkissoon <veronramkissoon@gmail.com>
2023-02-27 17:47:53 -0400

    Minimal changes

--- lib/pages/physioHomePage.dart
+++ lib/pages/physioHomePage.dart
@@ -1,7 +1,9 @@
+import 'package:finalyearproject/pages/patient_schedule.dart';
 import 'package:finalyearproject/pages/patients_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class PhysioHomePage extends StatefulWidget {
   const PhysioHomePage({super.key});
@@ -13,13 +15,28 @@ class PhysioHomePage extends StatefulWidget {
 class _HomePageState extends State<PhysioHomePage> {
   String _selectedOption = 'Schedule';
   Query dbpatientRef = FirebaseDatabase.instance.ref().child('Users');
+  DateTime today = DateTime.now();
 
+  Map<DateTime, List<dynamic>> _events = {
+    DateTime(2022, 3, 1): ['Event A', 'Event B'],
+    DateTime(2022, 3, 5): ['Event C'],
+    DateTime(2022, 3, 13): ['Event D', 'Event E'],
+  };
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     var container;
-    if (_selectedOption == "Patients"){
-        container = PatientsList();
+    if (_selectedOption == "Patients") {
+      container = PatientsList();
+    }
+    if (_selectedOption == "Schedule") {
+      container = PatientSchedule();
     }
     return Scaffold(
       appBar: AppBar(
@@ -50,10 +67,8 @@ class _HomePageState extends State<PhysioHomePage> {
               onTap: () {
                 setState(() {
                   _selectedOption = 'Patients';
-
                 });
                 Navigator.pop(context);
-
               },
             ),
             ListTile(
@@ -68,16 +83,19 @@ class _HomePageState extends State<PhysioHomePage> {
           ],
         ),
       ),
-      body: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white54,
-          centerTitle: true,
-          title: Text(_selectedOption, style: TextStyle(color: Colors.black),),
-        ),
-        body: Center(
-          child: container,
-        ),
+      // body: Scaffold(
+      //   appBar: AppBar(
+      //     backgroundColor: Colors.white54,
+      //     centerTitle: true,
+      //     title: Text(
+      //       _selectedOption,
+      //       style: TextStyle(color: Colors.black),
+      //     ),
+      //   ),
+      body: Center(
+        child: container,
       ),
+      //),
     );
   }
 }
