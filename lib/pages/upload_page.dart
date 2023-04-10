089c227187d0dd466b78d797d43cb9aa2293f962
selenamadray <77992492+selenamadray@users.noreply.github.com>
2023-04-09 21:26:09 -0400

    upload file

---
+++ lib/pages/upload_page.dart
@@ -0,0 +1,94 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:finalyearproject/components/upload_file.dart';
+import 'package:finalyearproject/widgets/button_widget.dart';
+
+
+class uploadPage extends StatefulWidget {
+  UploadTask? task;
+  File? file;
+
+  @override
+  Widget build(BuildContext context){
+    final fileName = file != null ? basename(file!.path): 'No File Selected';
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(MyApp.title),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),,
+        child; center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center
+            children: [
+              ButtonWidget(
+                text: 'Select File',
+                icon: Icons.attach_file,
+                onClicked: selectFile,
+              ),
+              SizedBox (height :8),
+              Text(
+                fileName,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 48),
+              ButtonWidget(
+                text: 'Upload File',
+                icon: Icons.cloud_upload_outlined,
+                onClicked: uploadFile,
+              ),
+              SizedBox(height: 20),
+              task != null ? buildUploadStatus(task!) : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() => file = File(path));
+  }
+
+  Future uploadFile() async{
+    if (file == null) return;
+
+    final fileName = basename(file!.path);
+    final destination = 'files/$fileName';
+
+    task = FirebaseUpload.uploadFile(destination, file!);
+    setState(() {});
+
+    if (task == null) return;
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    print('Download-Link: $urlDownload');
+  }
+
+  Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
+    stream: task.snapshotEvents,
+    builder: (context, snapshot){
+      if (snapshot.hasData){
+        final snap = snapshot.data!;
+        final progress = snap.bytesTransferred / snap.totalBytes;
+        final percentage = (progress * 100).toStringAsFixed(2);
+
+        return Text(
+          '$percentage %',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        );
+      } else {
+        return Container();
+      }
+    },
+  );
+}
