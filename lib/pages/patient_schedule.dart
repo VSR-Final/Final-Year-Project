dab74e6d1c837ce905befdffeab25d9ec2855902
Raul <raulgoindhan@gmail.com>
2023-04-17 21:35:29 -0400

    Exercise model update

--- lib/pages/patient_schedule.dart
+++ lib/pages/patient_schedule.dart
@@ -79,10 +79,10 @@ class _PatientScheduleState extends State<PatientSchedule> {
          from: doc.get('from').toDate(),
          to: doc.get('to').toDate(),
          isAllDay: doc.get('isAllDay'),
-          name: doc.get('physiotherapist_name'),
+          name: doc.get('patient_name'),
           exerciseID: doc.get('exerciseID'),
           appointmentID: doc.id,
-         isAppointment: doc.get('isAppointment'),
+         isAppointment: doc.get('isAppointment')
        );
 
 
